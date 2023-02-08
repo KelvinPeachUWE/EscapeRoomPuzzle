c8e9befe3f06cf39666a8e736942c197ea810169
Kelvin <kelvinpeachuwe@gmail.com>
2023-02-08 20:40:42 +0000

    Coded air vent cover

--- Assets/_Data/Scripts/Ingame/Environment/Interactables/AirVentCover.cs
+++ Assets/_Data/Scripts/Ingame/Environment/Interactables/AirVentCover.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class AirVentCover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("Cache")]
+    [SerializeField] Animator anim;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // DEBUG
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Use();
+        }
+        // DEBUG
+    }
+
+    void Use()
+    {
+        // Play animation
+        anim.SetTrigger("Open");
+
+        // Prevent being interacted with again
+        GetComponent<Collider>().enabled = false;
+        Destroy(this);
     }
 }
