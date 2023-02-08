4a637735c8588df21ffde3851446dd617c34ab40
Kelvin <kelvinpeachuwe@gmail.com>
2023-02-08 13:33:55 +0000

    Coded circuit breakers and laser beam grid

--- Assets/_Data/Scripts/Ingame/Environment/Interactables/CircuitBreaker.cs
+++ Assets/_Data/Scripts/Ingame/Environment/Interactables/CircuitBreaker.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CircuitBreaker : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Cache")]
+    [SerializeField] LaserBeamGrid laserBeamGrid;
+    [SerializeField] Animator anim;
+
+    bool isActivated;
+
+    // Events
+    public UnityEvent onActivated;
+    public UnityEvent onDeactivated;
+
+    public void Activate()
     {
-        
+        // Prevent the player trying to pull the lever when it's already activated
+        if (isActivated)
+            return;
+
+        // Prevent the player pulling the lever until the laser beam grid resets it
+        isActivated = true;
+
+        // Move the lever to the downward position
+        anim.SetTrigger("Activate");
+
+        // Disable laser beam grid
+        onActivated?.Invoke();
+
+        // Disable laser beam grid
+        // Keep this outside of activate otherwise you will get an infinite loop of the laser beam grid activating all the circuit breakers
+        //laserBeamGrid.Deactivate();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Deactivate()
     {
-        
+        if (!isActivated)
+            return;
+
+        // Allow the player to use this circuit breaker again
+        isActivated = false;
+
+        // Move the lever to the upward position
+        anim.SetTrigger("Deactivate");
+
+        // Enable laser beam grid
+        onDeactivated?.Invoke();
+        //laserBeamGrid.Activate();
     }
 }
--- Assets/_Data/Scripts/Ingame/Environment/Interactables/LaserBeamGrid.cs
+++ Assets/_Data/Scripts/Ingame/Environment/Interactables/LaserBeamGrid.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LaserBeamGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Settings")]
+    [SerializeField] float disableTime = 3f; // How long should the lasers stay deactivated for?
+    [SerializeField] GameObject[] lasers; // All the laser game objects to affect
+
+    // Events
+    public UnityEvent onActivated;
+    public UnityEvent onDeactivated;
+
+    bool isActivated = true;
+
+    public void Activate()
     {
-        
+        // Prevent being activated twice
+        if (isActivated)
+            return;
+
+        print("Laser beam grid activated.");
+
+        isActivated = true;
+
+        // Let all connected circuit breakers know they need to be activated
+        onActivated?.Invoke();
+
+        // Enable all the lasers so the player can't get through
+        foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(true);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Deactivate()
     {
-        
+        // Prevent being deactivated twice
+        if (!isActivated)
+            return;
+
+        print("Laser beam grid deactivated.");
+
+        isActivated = false;
+
+        // Let all connected circuit breakers know they need to be deactivated
+        onDeactivated?.Invoke();
+
+        // Disable all the lasers so the player can get through
+        foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(false);
+        }
+
+        // Start a timer to disable the lasers in a while
+        StartCoroutine(ResetCoroutine());
+    }
+
+    IEnumerator ResetCoroutine()
+    {
+        // Stay disabled for just long enough for a player to get through
+        yield return new WaitForSeconds(disableTime);
+
+        Activate();
     }
 }
