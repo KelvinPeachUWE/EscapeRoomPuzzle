a68190b8c7c23660540401730b331676f2706d4a
Kelvin <kelvinpeachuwe@gmail.com>
2023-02-16 21:39:01 +0000

    Added player robot models

---
+++ Assets/_Data/Scripts/Ingame/Environment/Interactables/HintInteractable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintInteractable : Interactable
+{
+    protected override void Use()
+    {
+        // Do nothing. Just display a message box.
+    }
+}
