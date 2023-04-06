6b166ab76d2f273a0f7eb3221c6f70c647fb24ab
Kelvin Peach <kelvinpeachuwe@gmail.com>
2023-04-06 14:52:51 +0100

    Air vent open particle effect

--- Assets/_Data/Scripts/Ingame/Environment/Interactables/AirVentCover.cs
+++ Assets/_Data/Scripts/Ingame/Environment/Interactables/AirVentCover.cs
@@ -7,11 +7,15 @@ public class AirVentCover : Interactable
 {
     [Header("Cache")]
     [SerializeField] Animator anim;
+    [SerializeField] ParticleSystem openEffect;
 
     protected override void Use(GameObject usedBy)
     {
         // Play animation
         anim.SetTrigger("Open");
+
+        // Play open impact particle effect
+        openEffect.Play();
 
         // Prevent being interacted with again
         GetComponent<Collider>().enabled = false;
